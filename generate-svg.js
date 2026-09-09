42f855a43622f31128c78ad77b797369dea3d06a
mshellw <130817300+mshellw@users.noreply.github.com>
2026-09-09 10:15:29 -0400

    Update generate-svg.js

--- generate-svg.js
+++ generate-svg.js
@@ -6,7 +6,7 @@ const BASE_ID = 'appPMuMIKkuahUkmG';
 async function generateSVG() {
   try {
 const response = await fetch(
-  `https://api.airtable.com/v0/${BASE_ID}/ACTIVE%20Panels?fields=ID&fields=Type&fields=Cut&pageSize=100`,
+  return { id, row: parseInt(row), col: parseInt(col), cut: r.fields.Cut_Boolean === 1 };
   { headers: { Authorization: `Bearer ${AIRTABLE_TOKEN}` } }
 );
 const data = await response.json();
